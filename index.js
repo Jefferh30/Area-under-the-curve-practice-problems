8e856e7e405813bed0338d59cf4c5b1960586c0a
Jefferson Huera <jefferh30@gmail.com>
2023-03-06 10:00:38 -0300

    Change applet and add function to inject app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const params = {"material_id":"uupndnkk", "borderColor":"#FFFFFF",};
-const ggbApplet = new GGBApplet(params, true);
-window.addEventListener("load", function() {     ggbApplet.setHTML5Codebase("https://www.geogebra.org/apps/5.0.498.0/web3d");
-        ggbApplet.inject('ggb-element');
-    });
+const injectApp = () => {
+    const params = {"material_id":"qm5hudcg", "borderColor":"#FFFFFF",};
+    const ggbApplet = new GGBApplet(params, true);
+    ggbApplet.setHTML5Codebase("https://www.geogebra.org/apps/5.0.498.0/web3d");
+    ggbApplet.inject('ggb-element');
+}
